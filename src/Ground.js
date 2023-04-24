26d6fa19cb678c2bc6a9074bf2dac49cd249d119
N S <kwb020312@naver.com>
2023-04-24 22:18:30 +0900

    Feat-Ground 추가

---
+++ src/Ground.js
@@ -0,0 +1,49 @@
+import { MeshReflectorMaterial } from "@react-three/drei";
+import { useLoader } from "@react-three/fiber";
+import { useEffect } from "react";
+import { LinearEncoding, RepeatWrapping, TextureLoader } from "three";
+
+const Ground = () => {
+  const [roughness, normal] = useLoader(TextureLoader, [
+    "textures/terrain-roughness.jpg",
+    "textures/terrain-normal.jpg",
+  ]);
+
+  useEffect(() => {
+    [normal, roughness].forEach((t) => {
+      t.wrapS = RepeatWrapping;
+      t.wrapT = RepeatWrapping;
+      t.repeat.set(5, 5);
+    });
+    normal.encoding = LinearEncoding;
+  }, [normal, roughness]);
+
+  return (
+    <mesh rotation-x={-Math.PI * 0.5} castShadow receiveShadow>
+      <planeGeometry args={[30, 30]} />
+      <MeshReflectorMaterial
+        envMapIntensity={0}
+        normalMap={normal}
+        normalScale={[0.15, 0.15]}
+        roughnessMap={roughness}
+        dithering={true}
+        color={[0.015, 0.015, 0.015]}
+        roughness={0.7}
+        blur={[1000, 400]}
+        mixBlur={30}
+        mixStrength={80}
+        mixContrast={1}
+        resolution={1024}
+        mirror={0}
+        depthScale={0.01}
+        minDepthThreshold={0.9}
+        maxDepthThreshold={1}
+        depthToBlurRatioBias={0.25}
+        debug={0}
+        reflectorOffset={0.2}
+      />
+    </mesh>
+  );
+};
+
+export default Ground;
--- src/index.js
+++ src/index.js
@@ -1,10 +1,5 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+import React from "react";
+import * as ReactDOM from "react-dom/client";
+import App from "./App";
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<App />);
