a97aa425c59543dad2021116726078e7e6bb31c0
N S <kwb020312@naver.com>
2023-04-25 22:12:23 +0900

    Feat-Box Object 그림자 반사도 추가

--- src/App.js
+++ src/App.js
@@ -11,6 +11,14 @@ import Ground from "./Ground";
 import Car from "./Car";
 import Rings from "./Rings";
 import { Boxes } from "./Boxes";
+import {
+  Bloom,
+  ChromaticAberration,
+  DepthOfField,
+  EffectComposer,
+} from "@react-three/postprocessing";
+import { BlendFunction } from "postprocessing";
+import FloatingGrid from "./FloatingGrid";
 
 function CarShow() {
   return (
@@ -28,7 +36,8 @@ function CarShow() {
       </CubeCamera>
 
       <Rings />
-
+      <Boxes />
+      <FloatingGrid />
       <spotLight
         color={[1, 0.25, 0.7]}
         intensity={1.5}
@@ -48,7 +57,27 @@ function CarShow() {
         shadow-bias={-0.0001}
       />
       <Ground />
-      <Boxes />
+      <EffectComposer>
+        <DepthOfField
+          focusDistance={0.0035}
+          focalLength={0.01}
+          bokehScale={3}
+          height={480}
+        />
+        <Bloom
+          blendFunction={BlendFunction.ADD}
+          intensity={1.3}
+          width={300}
+          height={300}
+          kernelSize={5}
+          luminanceThreshold={0.15}
+          luminanceSmoothing={0.025}
+        />
+        <ChromaticAberration
+          blendFunction={BlendFunction.NORMAL}
+          offset={[0.0005, 0.0012]}
+        />
+      </EffectComposer>
     </>
   );
 }
---
+++ src/FloatingGrid.js
@@ -0,0 +1,30 @@
+import { useLoader } from "@react-three/fiber";
+import React, { useEffect } from "react";
+import { RepeatWrapping, TextureLoader } from "three";
+
+const FloatingGrid = () => {
+  const diffuse = useLoader(TextureLoader, "textures/grid-texture.png");
+
+  useEffect(() => {
+    diffuse.wrapS = RepeatWrapping;
+    diffuse.wrapT = RepeatWrapping;
+    diffuse.anisotropy = 4;
+    diffuse.repeat.set(30, 30);
+    diffuse.offset.set(0, 0);
+  }, [diffuse]);
+
+  return (
+    <mesh rotation-x={-Math.PI * 0.5} position={[0, 0.425, 0]}>
+      <planeGeometry args={[35, 35]} />
+      <meshBasicMaterial
+        color={[1, 1, 1]}
+        opacity={0.15}
+        map={diffuse}
+        alphaMap={diffuse}
+        transparent={true}
+      />
+    </mesh>
+  );
+};
+
+export default FloatingGrid;
